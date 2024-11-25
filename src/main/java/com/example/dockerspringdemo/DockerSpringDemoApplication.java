f08d0329991fa9bb44caeec56201425cd175a6a0
ons-ou <onsouahchi@gmail.com>
2024-11-25 18:36:30 +0100

    Update DockerSpringDemoApplication.java

--- src/main/java/com/example/dockerspringdemo/DockerSpringDemoApplication.java
+++ src/main/java/com/example/dockerspringdemo/DockerSpringDemoApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerSpringDemoApplication {
 
 	public static void main(String[] args) {
-
-		
 		SpringApplication.run(DockerSpringDemoApplication.class, args);
 	}
 
